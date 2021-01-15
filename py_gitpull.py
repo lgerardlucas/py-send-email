0b8bf8bfa96bba9a4d79ead99865d0a26ef1d7c9
lgerardlucas <lgerardlucas@gmail.com>
2021-01-15 19:01:31 -0200

    Sistema de atualização por git

---
+++ py_gitpull.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+# -*- coding: latin-1 -*-
+# py_auditit.py = Arquivo de auditoria
+# Autor: Marcos Andre Gerard Alves - V1.00 - 04/10/2019
+
+# Modulo responsavel por coletar informacoes do banco host,
+# a fim de preparar e enviar os dados para o banco online, 
+# que serao contextualizado no corpo do e-mail pelo 
+# modulo py_email_bory.py
+import os
+teste = os.system("git pull")
+print(teste)
+
